5d97c3f16ae696290f6f29e7798b1d4f9148818f
Rodrigo <rodrigo.hammes7@gmail.com>
2020-11-23 19:35:31 -0300

    Estabelecida estrutura inicial do projeto

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>
+      <h1>Olá</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
